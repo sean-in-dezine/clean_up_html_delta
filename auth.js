a3ab5250de58a685e9a7a1749c14c54bf8e0bda3
sean-in-dezine <seankincaid777@gmail.com>
2020-07-06 06:21:08 -0500

    good stuff has come

--- auth.js
+++ auth.js
@@ -1,35 +1,129 @@
-//*relevancy: [@button, (4)submitting, (y)name] //_class
+// !selectors up here
 
-let click = document.querySelector('.click')
-/*.click{}*/
-//*relevancy: [@button, (4)submitting, (y)name] //_id
-let id_name = document.getElementById('id_name')
-/*#id_name{}*/
-id_name.addEventListener('click', clickEvent)
+// !top down starting here:
+let container_main = document.querySelector('.container_main')
 
-function clickEvent(ev) {
-    ev.preventDefault()
-}
+let login_sub_cntr = document.querySelector('.login_sub_cntr')
 
 
-//*relevancy: [@submit, (4)login, (y)entry] //_class
-// !emmet
-// button.submit_id_name_bt_cntr.login_height_adjustments#submit_id_nmae
+let create_login = document.querySelector('create_login')
+let create_login_other = document.querySelector('create')
+let create__id = document.getElementById('create_login')
+let blocks1 = document.querySelector('blocks1')
+let forgot = document.querySelector('forgot')
+let request = document.querySelector('request')
+let request_login_button = document.getElementById('request_login')
+let forgot_id = document.getElementById('forgot_id')
 
-// !css
-/*.submit_id_name_bt_cntr{}*/
-/*#submit_id_name{}*/
 
-// !js
+let tabulated = document.querySelector('.tabulated')
+let tabular = document.querySelector('.tabular')
+let row = document.querySelector('.row')
+
+// !top down ending here
+
+
+// !javascript territory at the bottom
+
+let create_login = document.querySelector('create_login')
+let create_login_other = document.querySelector('create')
+let create__id = document.getElementById('create_login')
+let blocks1 = document.querySelector('blocks1')
+let forgot = document.querySelector('forgot')
+let request = document.querySelector('request')
+let request_login_button = document.getElementById('request_login')
+let forgot_id = document.getElementById('forgot_id')
+
+
+let tabulated = document.querySelector('.tabulated')
+let tabular = document.querySelector('.tabular')
+let row = document.querySelector('.row')
+
+let email_in_cntr = document.querySelector('.email_in_cntr')
+let email = email_in_cntr
+email.name = 'email'
+email.required = 'true'
+email.placeholder = 'your@email.domain'
+let label_email = document.createElement('label')
+label_email.htmlFor = 'email'
+label_email.id = 'label_email_id'
+email.parentNode.append(label_email)
+let = document.querySelector('.email_in_cntr')
+let = document.getElementById('email_id')
+
+
+let password_in_cntr = document.querySelector('.password_in_cntr')
+let password = document.getElementById('password')
+password.name = 'password'
+password.required = 'true'
+password.placeholder = 'password'
+let label_password = document.createElement('label')
+label_password.htmlFor = 'password'
+label_password.id = 'label_password_id'
+password.parentNode.append(label_password)
+
 let submit_id_name_bt_cntr = document.querySelector('.submit_id_name_bt_cntr')
 let login_height_adjustments = document.querySelector('.login_height_adjustments')
 let submit_id_name = document.getElementById('submit_id_name')
 
+let register_sub_cntr = document.querySelector('.register_sub_cntr')
+let display_register_dv_cntr = document.querySelector('.display_register_dv_cntr')
+let or_register = document.querySelector('.or_register')
+let register_from_login = document.querySelector('.register_from_login')
+let register_from_login_id = document.getElementById('register_from_login_id')
+let convincing_argument = document.querySelector('convincing_argument')
+
+let register_sub_cntr = document.querySelector('.register_sub_cntr')
+let sub_register_id = document.getElementById('sub_register_id')
+
+
+let register_button_dv_cntr = document.querySelector('register_button_dv_cntr')
+let blocksfive = document.querySelector('.blocks5')
+let blocks = document.querySelector('.blocks')
+
+let email_in_cntr = document.querySelector('.email_in_cntr')
+let username = document.getElementById('username')
+
+
+let blocks1 = document.querySelector('.blocks1')
+let forgot = document.querySelector('.forgot')
+let request = document.querySelector('.request')
+let request_login_button = document.getElementById('.request_login')
+let forgot_id = document.getElementById('forgot_id')
+
+let submit_users = document.querySelector('.submit_users')
+let user_id = document.getElementById('user')
+let not_so_wide = document.querySelector('.not_so_wide')
+let input_class = document.querySelector('.input')
+let submit_users_id = document.getElementById('submit_users_id')
+
+
+let tabulated = document.querySelector('.tabulated')
+let tabular = document.querySelector('.tabular')
+let row = document.querySelector('.row')
+
+
+// !selectors end
+
+
+// !event listeners
+
+
 submit_id_name.addEventListener('click', loginAttempt)
+
 
 function loginAttempt(ev) {
     ev.preventDefault()
 }
 
+
+
+register_from_login_id.addEventListener('click', registerFromLogin)
+
+function registerFromLogin(ev) {
+    ev.preventDefault()
+}
+
+// !event listeners end
 
 
